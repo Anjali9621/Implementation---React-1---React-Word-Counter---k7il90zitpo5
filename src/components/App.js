2461402a854cdda8fa78cf9b361f98ca49f34eaf
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-06-01 21:07:12 +0530

    str

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-
+  const [text, settext] = useState("");
+  const [max, setmax] = useState(50);
 
   return (
     <div id="main">
+      <textarea
+        maxlength={max}
+        onChange={(e) => settext(e.target.value)}
+      ></textarea>
+      <div className="word-counter">{text.split(" ").length}</div>
+      <div id="char-counter">{text.length}</div>
+      <input
+        type="number"
+        value={max}
+        onChange={(e) => e.target.value}
+        id="char-limit-input"
+      />
+      <input type="range" max="32px" min="16" />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
