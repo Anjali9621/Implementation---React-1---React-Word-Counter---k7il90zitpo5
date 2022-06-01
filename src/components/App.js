756fc6a0a71dc1f3c9b91cbba59ae590f3bca528
Anjali9621 <95613922+Anjali9621@users.noreply.github.com>
2022-06-01 21:19:10 +0530

    str1

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "../styles/App.css";
 const App = () => {
   const [text, settext] = useState("");
-  const [max, setmax] = useState(50);
+  const [max, setmax] = useState(84);
 
   return (
     <div id="main">
@@ -10,14 +10,22 @@ const App = () => {
         maxlength={max}
         onChange={(e) => settext(e.target.value)}
       ></textarea>
-      <div className="word-counter">{text.split(" ").length}</div>
-      <div id="char-counter">{text.length}</div>
+      <div className="word-counter">
+        <p>Total number of words written {text.split(" ").length}</p>
+      </div>
+      <div id="char-counter">
+        <p>Total number of charters used {text.length}</p>
+      </div>
+      <br />
+      <h3>Word Limit Input</h3>
       <input
         type="number"
         value={max}
         onChange={(e) => e.target.value}
         id="char-limit-input"
       />
+      <br />
+      <h3>Font Size Picker</h3>
       <input type="range" max="32px" min="16" />
     </div>
   );
